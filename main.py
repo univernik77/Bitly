f8a9e6caf0cafad6f0abafe4a79af451d21c2692
univernik77 <146747152+univernik77@users.noreply.github.com>
2023-10-20 12:44:01 +0300

    Отформатировал код
    Отформатирован main согласно PEP8 и рекомендациям Девман

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ import os
 
 def is_bitlink(token, url):
     parsed_url = urlparse(url)
-    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{parsed_url.netloc}{parsed_url.path}',
-                            headers={'Authorization': f'Bearer {token}'})
+    response = requests.get(
+        f'https://api-ssl.bitly.com/v4/bitlinks/'
+        f'{parsed_url.netloc}{parsed_url.path}',
+        headers={'Authorization': f'Bearer {token}'}
+    )
     return response.ok
 
 
 def shorten_link(token, url):
     payload = {"long_url": url}
-    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks',
-                             headers={'Authorization': f'Bearer {token}'},
-                             json=payload)
+    response = requests.post(
+        'https://api-ssl.bitly.com/v4/bitlinks',
+        headers={'Authorization': f'Bearer {token}'},
+        json=payload
+    )
     response.raise_for_status()
     return response.json()['link']
 
@@ -24,8 +29,11 @@ def shorten_link(token, url):
 def count_clicks(token, link):
     parsed_url = urlparse(link)
     response = requests.get(
-        f'https://api-ssl.bitly.com/v4/bitlinks/{parsed_url.netloc}{parsed_url.path}/clicks/summary',
-        headers={'Authorization': f'Bearer {token}'})
+        f'https://api-ssl.bitly.com/v4/bitlinks/'
+        f'{parsed_url.netloc}{parsed_url.path}'
+        f'/clicks/summary',
+        headers={'Authorization': f'Bearer {token}'}
+    )
     response.raise_for_status()
     return response.json()['total_clicks']
 
@@ -33,12 +41,15 @@ def count_clicks(token, link):
 def main():
     load_dotenv()
     bitly_token = os.environ['BITLY_TOKEN']
-    parser = argparse.ArgumentParser(description='Возвращает короткую ссылку или количество переходов')
+    parser = argparse.ArgumentParser(
+        description='Возвращает короткую ссылку или количество переходов'
+    )
     parser.add_argument('url', help='Введите ссылку или короткую ссылку')
     input_url = parser.parse_args().url
     try:
         if is_bitlink(bitly_token, input_url):
-            print(f'По вашей ссылке перешли {count_clicks(bitly_token, input_url)} раз(а)')
+            print(f'По вашей ссылке перешли '
+                  f'{count_clicks(bitly_token, input_url)} раз(а)')
         else:
             print(f'Битлинк: {shorten_link(bitly_token, input_url)}')
     except requests.exceptions.HTTPError:
